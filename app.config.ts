36ecdc611656beb82d4aec29399d35c5524d20b2
dimnissv <dimnissv@yandex.kz>
2025-08-11 20:03:14 +0500

    style(ui): toasts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,5 @@
 // app.config.ts
 export default defineAppConfig({
-  // ★ ИЗМЕНЕНИЕ: Обновляем заголовок
   title: 'jCloud.me',
   worker: {
     tempDir: './temp',
@@ -10,10 +9,24 @@ export default defineAppConfig({
     ffprobe: 'ffprobe',
     ytdlp: 'yt-dlp',
   },
+  toaster: {
+    position: 'bottom-left' as const,
+    expand: true,
+    duration: 5000
+  },
   ui: {
     toast: {
-      "slots": {
-        "root": "flex z-[100] w-80 m-4 bg-white group overflow-hidden shadow-lg rounded-lg ring ring-default p-4 gap-3 focus:outline-none",
+      slots: {
+        root: 'min-w-80 overflow-wrap z-[100] bg-white shadow-lg rounded-lg ring ring-default p-4 m-4 m-bottom-10 flex gap-2.5 focus:outline-none',
+        wrapper: 'w-0 h-0 flex-1 flex flex-col',
+        title: 'text-sm font-medium text-highlighted',
+        description: 'text-sm text-muted',
+        icon: 'shrink-0 size-5',
+        avatar: 'shrink-0',
+        avatarSize: '2xl',
+        actions: 'flex gap-1.5 shrink-0',
+        progress: 'absolute inset-x-0 bottom-0',
+        close: 'p-0'
       },
     },
     modal: {
